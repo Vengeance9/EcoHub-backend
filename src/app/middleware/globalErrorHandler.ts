a6188be72ffb3ca46665206158c164f9a61ff3da
vengeance9 <mustakimabtahi207@gmail.com>
2026-05-09 02:02:40 +0600

    added global error handler middleware

---
+++ src/app/middleware/globalErrorHandler.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from "express";
+import { TErrorResponse, TErrorSources } from "../interfaces/errorInterface";
+import status from "http-status";
+import z from "zod";
+import { handleZodError } from "../errorHelpers/handleZodError";
+import AppError from "../errorHelpers/AppError";
+
+
+const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): TErrorResponse => {
+  let statusCode: number = status.INTERNAL_SERVER_ERROR;
+  let message = "Something went wrong";
+  let stack: string | undefined = undefined;
+  let errorSources: TErrorSources[] = [];
+
+  if (err instanceof z.ZodError) {
+    const zodError = handleZodError(err);
+    message = zodError.message;
+    statusCode = zodError.statusCode;
+    errorSources = zodError.errorSources;
+  } else if (err instanceof AppError) {
+    statusCode = err.statusCode;
+    message = err.message;
+    stack = err.stack;
+    errorSources = [{ path: "", message: err.message }];
+  } else if (err instanceof Error) {
+    statusCode = status.BAD_REQUEST;
+    message = err.message;
+    stack = err.stack;
+    errorSources = [{ path: "", message: err.message }];
+  }
+
+  return {
+    success: false,
+    message,
+    statusCode,
+    errorSources,
+    stack: stack || undefined,
+  };
+};
